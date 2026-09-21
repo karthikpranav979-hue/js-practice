4f17c7c79d248e604cd8268f7c567017094f163f
Pranav Karthik <karthikpranav979@gmail.com>
2026-09-21 17:32:18 +0530

    variables.js added

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const a=9;
+let b="bi";
+var c="ci";
+let d="di";
+console.table([a,b,c,d]);
+let h;
+function sayHi() {
+  phrase = "Hello";
+  globalThis.h="hey";
+  console.log(phrase);
+  if(false){
+    var phrase;
+    
+  }
+}
+// sayHi();
+console.log(h);
